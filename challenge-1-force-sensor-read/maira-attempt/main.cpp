85e963e7c7d0f19eea0f1ec76e9fb2e4ca59a038
mairacanal <maira.canal@usp.br>
2021-02-19 09:04:54 -0300

    fixing typos and variable types

--- challenge-1-force-sensor-read/maira-attempt/main.cpp
+++ challenge-1-force-sensor-read/maira-attempt/main.cpp
@@ -3,7 +3,7 @@
 Challenge 1 - Force/Torque sensor read
 
 * @file main.cpp
-* @brief Exercise for the modern C++ study group at AeroTech Lab, School of 
+* @brief Exercise for the modern C++ study group at AeroTech Lab, School of
 Engineering of São Carlos - University of São Paulo - Brazil
 * @author Autor: Maíra Canal (@mairacanal)
 * @date Jan 2021
@@ -12,17 +12,15 @@ Engineering of São Carlos - University of São Paulo - Brazil
 *********************************************************************/
 
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <vector>
 #include <array>
-#include "../force_torque_to_file.h" 
+#include "force_torque_to_file.h"
 
 #define READ_AXIS_PACKAGE_SIZE 1
 #define CALIBRATION_MATRIX_SIZE 6
 #define SENSOR_LOG_LINE_SIZE 43
 
-/* Data structure defined to convert 4-bytes floating point 
+/* Data structure defined to convert 4-bytes floating point
 in hexadecimal format to a 4-bytes floating point in decimal
 format*/
 union ulf {
@@ -74,7 +72,7 @@ class CANresponse {
          */
         void get_payload(std::string line) {
 
-            payloadSize = line[15] -'0';    
+            payloadSize = line[15] -'0';
 
         }
         /*
@@ -92,7 +90,7 @@ class CANresponse {
                 bytes.push_back(byte);
                 it = it + 2;
 
-            } 
+            }
 
         }
 
@@ -105,7 +103,7 @@ class Sensor {
         CANresponse data;
 
         /*
-         * @brief Converts a 4-bytes floating point in hexadecimal format into a 
+         * @brief Converts a 4-bytes floating point in hexadecimal format into a
          * 4-bytes floating point in decimal format
          * @param 4-bytes floating point in hexadecimal format
          * @return 4-bytes floating point in decimal format
@@ -121,14 +119,14 @@ class Sensor {
 
         }
 
-    public: 
+    public:
 
         int CpF;
         int CpT;
         std::string forceUnit;
         std::string torqueUnit;
-        int actualReadingAxis;
-        std::array<int,CALIBRATION_MATRIX_SIZE> SG;
+        int currentReadingAxis;
+        std::array<int16_t,CALIBRATION_MATRIX_SIZE> SG;
         std::array<std::array<float, CALIBRATION_MATRIX_SIZE>, CALIBRATION_MATRIX_SIZE> calibrationMatrix;
         std::vector<std::array<float, 3>> force{};
         std::vector<std::array<float, 3>> torque{};
@@ -181,15 +179,15 @@ class Sensor {
 
         /*
          * @brief Read SG Data => A packet with the opcode set to b0000, which
-         * contains the two byte status code, followed by the two byte values 
+         * contains the two byte status code, followed by the two byte values
          * for sg0, sg2, and sg4
          * @param 8-bytes vector in hexadecimal format
          */
         void read_0x0(std::vector<std::string> bytes) {
 
-            SG[0] = std::stoi("0x" + bytes[2] + bytes[3], nullptr, 16);
-            SG[2] = std::stoi("0x" + bytes[4] + bytes[5], nullptr, 16);
-            SG[4] = std::stoi("0x" + bytes[6] + bytes[7], nullptr, 16);
+            SG[0] = std::stoul("0x" + bytes[2] + bytes[3], nullptr, 16);
+            SG[2] = std::stoul("0x" + bytes[4] + bytes[5], nullptr, 16);
+            SG[4] = std::stoul("0x" + bytes[6] + bytes[7], nullptr, 16);
 
         }
 
@@ -200,9 +198,9 @@ class Sensor {
          */
         void read_0x1(std::vector<std::string> bytes) {
 
-            SG[1] = std::stoi("0x" + bytes[0] + bytes[1], nullptr, 16);
-            SG[3] = std::stoi("0x" + bytes[2] + bytes[3], nullptr, 16);
-            SG[5] = std::stoi("0x" + bytes[4] + bytes[5], nullptr, 16);
+            SG[1] = std::stoul("0x" + bytes[0] + bytes[1], nullptr, 16);
+            SG[3] = std::stoul("0x" + bytes[2] + bytes[3], nullptr, 16);
+            SG[5] = std::stoul("0x" + bytes[4] + bytes[5], nullptr, 16);
 
         }
 
@@ -215,22 +213,22 @@ class Sensor {
 
             if (bytes.size() == READ_AXIS_PACKAGE_SIZE) {
 
-                actualReadingAxis = std::stoi("0x" + bytes[0], nullptr, 16);
+                currentReadingAxis = std::stoi("0x" + bytes[0], nullptr, 16);
 
             } else {
 
                 float sg0 = fromHEXtofloat("0x" + bytes[0] + bytes[1] + bytes[2] + bytes[3]);
                 float sg1 = fromHEXtofloat("0x" + bytes[4] + bytes[5] + bytes[6] + bytes[7]);
 
-                if (actualReadingAxis == Axis::Fx || actualReadingAxis == Axis::Fy || actualReadingAxis == Axis::Fz) {
+                if (currentReadingAxis == Axis::Fx || currentReadingAxis == Axis::Fy || currentReadingAxis == Axis::Fz) {
 
-                    calibrationMatrix[actualReadingAxis][0] = sg0/CpF;
-                    calibrationMatrix[actualReadingAxis][1] = sg1/CpF;
+                    calibrationMatrix[currentReadingAxis][0] = sg0/CpF;
+                    calibrationMatrix[currentReadingAxis][1] = sg1/CpF;
 
                 } else {
 
-                    calibrationMatrix[actualReadingAxis][0] = sg0/CpT;
-                    calibrationMatrix[actualReadingAxis][1] = sg1/CpT;
+                    calibrationMatrix[currentReadingAxis][0] = sg0/CpT;
+                    calibrationMatrix[currentReadingAxis][1] = sg1/CpT;
 
                 }
 
@@ -244,18 +242,18 @@ class Sensor {
          */
         void read_0x3(std::vector<std::string> bytes) {
 
-            float sg2 = fromHEXtofloat("0x" + bytes[0] + bytes[1] + bytes[2] + bytes[3]); 
+            float sg2 = fromHEXtofloat("0x" + bytes[0] + bytes[1] + bytes[2] + bytes[3]);
             float sg3 = fromHEXtofloat("0x" + bytes[4] + bytes[5] + bytes[6] + bytes[7]);
 
-            if (actualReadingAxis == Axis::Fx || actualReadingAxis == Axis::Fy || actualReadingAxis == Axis::Fz) {
+            if (currentReadingAxis == Axis::Fx || currentReadingAxis == Axis::Fy || currentReadingAxis == Axis::Fz) {
 
-                calibrationMatrix[actualReadingAxis][2] = sg2/CpF;
-                calibrationMatrix[actualReadingAxis][3] = sg3/CpF;
+                calibrationMatrix[currentReadingAxis][2] = sg2/CpF;
+                calibrationMatrix[currentReadingAxis][3] = sg3/CpF;
 
             } else {
 
-                calibrationMatrix[actualReadingAxis][2] = sg2/CpT;
-                calibrationMatrix[actualReadingAxis][3] = sg3/CpT;
+                calibrationMatrix[currentReadingAxis][2] = sg2/CpT;
+                calibrationMatrix[currentReadingAxis][3] = sg3/CpT;
 
             }
 
@@ -267,18 +265,18 @@ class Sensor {
          */
         void read_0x4(std::vector<std::string> bytes) {
 
-            float sg4 = fromHEXtofloat("0x" + bytes[0] + bytes[1] + bytes[2] + bytes[3]); 
+            float sg4 = fromHEXtofloat("0x" + bytes[0] + bytes[1] + bytes[2] + bytes[3]);
             float sg5 = fromHEXtofloat("0x" + bytes[4] + bytes[5] + bytes[6] + bytes[7]);
 
-            if (actualReadingAxis == Axis::Fx || actualReadingAxis == Axis::Fy || actualReadingAxis == Axis::Fz) {
+            if (currentReadingAxis == Axis::Fx || currentReadingAxis == Axis::Fy || currentReadingAxis == Axis::Fz) {
 
-                calibrationMatrix[actualReadingAxis][4] = sg4/CpF;
-                calibrationMatrix[actualReadingAxis][5] = sg5/CpF;
+                calibrationMatrix[currentReadingAxis][4] = sg4/CpF;
+                calibrationMatrix[currentReadingAxis][5] = sg5/CpF;
 
             } else {
 
-                calibrationMatrix[actualReadingAxis][4] = sg4/CpT;
-                calibrationMatrix[actualReadingAxis][5] = sg5/CpT;
+                calibrationMatrix[currentReadingAxis][4] = sg4/CpT;
+                calibrationMatrix[currentReadingAxis][5] = sg5/CpT;
 
             }
 
@@ -293,9 +291,9 @@ class Sensor {
             }
 
         }
-        
+
         /*
-         * @brief Defines counts per force (CpF) and counts per torque (CpT) 
+         * @brief Defines counts per force (CpF) and counts per torque (CpT)
          * The first 4 bytes are the counts per force, followed by the 4 byte
          * counts per torque.
          * @param 8-bytes vector in hexadecimal format
@@ -330,12 +328,12 @@ class Sensor {
             force.emplace_back(std::array<float,3> {0,0,0});
             torque.emplace_back(std::array<float,3> {0,0,0});
 
-            for (int i = 0; i < CALIBRATION_MATRIX_SIZE; i++) {
+            for (int i = 0; i < CALIBRATION_MATRIX_SIZE; ++i) {
 
-                for (int j = 0; j < CALIBRATION_MATRIX_SIZE; j++) {
+                for (int j = 0; j < CALIBRATION_MATRIX_SIZE; ++j) {
 
-                    if (i < 3) force[force.size() - 1][i] += calibrationMatrix[i][5 - j] * SG[j];
-                    else torque[torque.size() - 1][i - 3] += calibrationMatrix[i][5 - j] * SG[j];
+                    if (i < 3) force[force.size() - 1][i] += calibrationMatrix[i][j] * SG[j];
+                    else torque[torque.size() - 1][i - 3] += calibrationMatrix[i][j] * SG[j];
 
                 }
 
@@ -350,10 +348,10 @@ int main () {
     std::fstream file;
     Sensor sensor;
 
-    file.open("sensor-log.txt", std::ios::in);
+    file.open("../sensor-log.txt", std::ios::in);
 
     if (!file) {
-        
+
         std::cout << "File not finded" << std::endl;
 
     } else {
